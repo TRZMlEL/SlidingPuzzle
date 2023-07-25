69527bb8af67b0aaa3a15d3b310a04b9a1a1b098
TRZMlEL <mateusztrzmiel2104@gmail.com>
2023-07-25 14:22:49 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,14 @@
+const gameTiles = document.querySelectorAll('.tile');
+
+const gameBoard = document.querySelector('#gameBoard');
+
+const gameState = [
+    gameTiles[0], gameTiles[1], gameTiles[2], 
+    gameTiles[3], gameTiles[4], gameTiles[5], 
+    gameTiles[6], gameTiles[7], gameTiles[8]
+];
+
+
+gameBoard.addEventListener('click', () => {
+
+})
