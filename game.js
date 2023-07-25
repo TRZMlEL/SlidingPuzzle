647f2f3cc12b4231c4f690616eb2ca5d4ea9bb6b
TRZMlEL <mateusztrzmiel2104@gmail.com>
2023-07-26 01:23:55 +0200

    modified:   game.js 	modified:   style.css

--- game.js
+++ game.js
@@ -3,12 +3,54 @@ const gameTiles = document.querySelectorAll('.tile');
 const gameBoard = document.querySelector('#gameBoard');
 
 const gameState = [
-    gameTiles[0], gameTiles[1], gameTiles[2], 
-    gameTiles[3], gameTiles[4], gameTiles[5], 
-    gameTiles[6], gameTiles[7], gameTiles[8]
+    [gameTiles[0], gameTiles[1], gameTiles[2]], 
+    [gameTiles[3], gameTiles[4], gameTiles[5]], 
+    [gameTiles[6], gameTiles[7], gameTiles[8]]
 ];
 
+function render(gameBoard, gameState) {
+    gameState.forEach((row) =>{
+        row.forEach((column) => {
+            gameBoard.appendChild(column);
+        });
+    });
+};
 
-gameBoard.addEventListener('click', () => {
 
-})
+gameBoard.addEventListener('click', (event) => {
+    const target = event.target;
+
+    let x, y;
+
+    gameState.forEach((row, rowIndex) => {
+        row.forEach((column, columnIndex) => {
+            if(column === target){
+                x = rowIndex;
+                y = columnIndex;
+            };
+        });
+    });
+
+// -------------------
+
+    let emptyX, emptyY;
+
+    gameState.forEach((row, rowIndex) => {
+        row.forEach((column, columnIndex) => {
+            if(column.innerText === ''){
+                emptyX = rowIndex;
+                emptyY = columnIndex;
+            };
+        });
+    });
+
+    if(
+    (y === emptyY && (x + 1 === emptyX || x - 1 === emptyX)) || 
+    (x === emptyX && (y + 1 === emptyY || y - 1 === emptyY))
+    ){
+        const temp = gameState[x][y];
+        gameState[x][y] = gameState[emptyX][emptyY];
+        gameState[emptyX][emptyY] = temp;
+        render(gameBoard, gameState);
+    };
+});
